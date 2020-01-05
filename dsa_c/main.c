77b8264734de85325f6627fa7134350622385a5f
Aakash <aakashk.kvjp58@gmail.com>
2020-01-05 15:29:32 +0530

    Recursive method

--- dsa_c/main.c
+++ dsa_c/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "search_iterative.h"
+#include "BinSearch.h"
 
 int main() {
     struct Array arr = {(int*)malloc(10* sizeof(int)), 10};
-    for (int i=0 ; i < 10; i++)
-    {
-        arr.arr[i] = i ;
-    }
-    printf("Index: %d", binSearch(&arr,33));
+    //             {0,1,2,4,4 ,5 ,6 ,7 ,8 ,9 }
+    int temp[10] = {2,6,7,9,23,45,56,78,98,234};
+    arr.arr = temp;
+    printf("Index(Iterative): %d\n", BinSearch_iterative(&arr,98));
+    printf("Index(Recursive): %d\n", BinSearch_iterative(&arr,98));
     return 0;
 }
