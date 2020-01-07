1ae2b3d4e737d2a63be46bdec368288e1ea57df0
Aakash <aakashk.kvjp58@gmail.com>
2020-01-07 22:55:56 +0530

    Update main.cpp

--- ds_cpp/main.cpp
+++ ds_cpp/main.cpp
@@ -2,46 +2,38 @@
 #include "linked_list.h"
 #include "linked_list.cpp"
 #include "p_match.h"
+#include "queue.h"
+//#include "queue.cpp"
+#include "graph.h"
+#include <vector>
+//#include "graph.cpp"
 
+#define println(x) std::cout << x << std::endl
 using namespace std;
 
 int main()
 {
-//    auto list = linked_list<int >(2);
-//    list.push(3);
-//    list.push(4);
-//    list.insert_as_head(1);
-//    list.insert_as_head(0);
-//
-//    for(int x ; !list.is_empty(); ){
-//        cout << "count:" << list.get_count();
-//        x = list.delete_head();
-//        cout << " "<< x << endl;
+//    char x;
+//    cout << "Enter equation, finish it by \'!\'";
+//    cin >> x;
+//    auto exp = stack<char>(x);
+//    while (true)
+//    {
+//        cin >> x;
+//        if (x == '!')
+//            break;
+//        exp.push(x);
+//        // cout<< "Count:" << exp.count() <<endl;
 //    }
-//
-//    list.insert_as_head(5);
-//    list.insert_as_head(-2);
-//    list.push(8);
-//    list.insert_as_head(-4);
-
-//    cout<< list.delete_head();
-//    for(auto x = list.delete_head(); !list.is_empty(); ){
-//        x = list.delete_head();
-//        cout << x << endl;
-//    }
-
-    char x;
-    cout << "Enter equation, finish it by \'!\'";
-    cin >> x;
-    auto exp = stack<char>(x);
-    while (true)
-    {
-        cin >> x;
-        if (x == '!')
-            break;
-        exp.push(x);
-        // cout<< "Count:" << exp.count() <<endl;
-    }
-    if(p_match(exp)) cout << "Valid Eqn";
-
+//    if(p_match(exp)) cout << "Valid Eqn";
+auto x = queue<char>('A');
+x.enq_front('B');
+x.enq_front('C');
+x.enq_front('D');
+x.enq_rear('X');
+x.enq_rear('Y');
+cout<< x.get_front() << ' ' << x.get_rear() << endl;
+x.deq_front();x.deq_front();
+x.deq_rear();
+cout<< x.get_front() << ' ' << x.get_rear();
 }
